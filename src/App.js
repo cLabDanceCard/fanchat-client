4d281277da434f3b5ca1e7c324619eddf8f1bc53
dannyl1u <dannyjialiliu@gmail.com>
2023-11-08 11:03:30 -0800

    get caller id

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import Peer from 'peerjs';
 
 function App() {
+  const [myId, setMyId] = useState('');
+  const [callId, setCallId] = useState('');
+  const [myStream, setMyStream] = useState(null);
+  const myAudio = useRef(null);
+  const peerRef = useRef(null);
+
+  useEffect(() => {
+    // Access user's microphone
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then((stream) => {
+        setMyStream(stream);
+      })
+      .catch((err) => {
+        console.error('Failed to get audio stream:', err);
+      });
+
+    // Initialize Peer object
+    peerRef.current = new Peer(undefined, {
+      host: 'localhost',
+      port: 5000,
+      path: '/peerjs',
+      secure: false,
+    });
+
+    peerRef.current.on('open', (id) => {
+      console.log('PeerJS on open with ID:', id);
+      setMyId(id);
+    });
+
+    peerRef.current.on('error', (err) => {
+      console.error('PeerJS error:', err);
+    });
+
+    return () => {
+      peerRef.current.destroy();
+    };
+  }, []);
+
+  useEffect(() => {
+    // Listen for incoming calls
+    if (peerRef.current) {
+      peerRef.current.on('call', (call) => {
+        call.answer(myStream); 
+        call.on('stream', (remoteStream) => {
+          myAudio.current.srcObject = remoteStream;
+        });
+      });
+    }
+  }, [myStream]);
+
+  const makeCall = (id) => {
+    if (myStream) {
+      const call = peerRef.current.call(id, myStream);
+      call.on('stream', (remoteStream) => {
+        myAudio.current.srcObject = remoteStream;
+      });
+    } else {
+      console.error('Audio stream not available');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Your ID: {myId}</h2>
+      <input
+        value={callId}
+        onChange={(e) => setCallId(e.target.value)}
+        placeholder="Enter ID to call"
+      />
+      <button onClick={() => makeCall(callId)}>Call</button>
+      <audio ref={myAudio} controls></audio>
     </div>
   );
 }
