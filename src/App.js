5037f2a126c4a5ed4e5f0a4fea1c5876b5628489
dannyl1u <dannyjialiliu@gmail.com>
2023-11-09 12:55:14 -0800

    fix repeated api calls

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Peer from 'peerjs';
 
 function App() {
   const [myId, setMyId] = useState('');
-  const [peer] = useState(new Peer(undefined, {host: process.env.REACT_APP_PEERJS_URL, path: '/peerjs'}));
-
+  const [peer] = useState(new Peer(undefined, {
+    host: process.env.REACT_APP_PEERJS_URL,
+    path: '/peerjs'
+  }));
   const [myStream, setMyStream] = useState(null);
   const [isWaitingForPeer, setIsWaitingForPeer] = useState(false);
-
   const myAudio = useRef(null);
 
   useEffect(() => {
@@ -20,66 +21,65 @@ function App() {
       })
       .catch(err => console.error('Failed to get local stream', err));
 
-    let isCancelled = false;
-
-    peer.on('open', id => {
-      if (!isCancelled) {
+    const registerPeerEvents = () => {
+      peer.on('open', id => {
         console.log('My peer ID is: ', id);
         setMyId(id);
-        setIsWaitingForPeer(true);
+        setIsWaitingForPeer(true); // Start waiting for a peer
+        // Notify the server that this user is waiting
         fetch(`${process.env.REACT_APP_PEERJS_URL}/wait`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({ peerId: id })
-        }).then(() => {
-          if (!isCancelled) {
-            getPeerToCall();
-          }
         });
-      }
-    });
+      });
 
-    peer.on('call', call => {
-      call.answer(myStream);
-      call.on('stream', handleStream);
-    });
+      peer.on('call', call => {
+        call.answer(myStream);
+        call.on('stream', handleStream);
+      });
+    };
+
+    registerPeerEvents();
 
     // Clean up on unmount
     return () => {
-      isCancelled = true;
       myStream?.getTracks().forEach(track => track.stop());
+      peer.destroy(); // Ensures we clean up event listeners and peer connections
     };
-  }, [peer, myStream]);
+  }, [peer]); // Removed myStream dependency to avoid re-registering peer events on stream changes
 
   useEffect(() => {
-    if (myId && !isWaitingForPeer) {
+    if (myId && isWaitingForPeer) {
       getPeerToCall();
     }
-  }, [myId, isWaitingForPeer])
+  }, [myId, isWaitingForPeer]);
 
   const getPeerToCall = async () => {
+    // Prevent multiple calls
+    if (isWaitingForPeer) return;
 
-    if (isWaitingForPeer) {
-      return;
-    }
     setIsWaitingForPeer(true);
     try {
       const res = await fetch(`${process.env.REACT_APP_PEERJS_URL}/pair`);
       if (res.status === 200) {
         const { peerId } = await res.json();
         makeCall(peerId);
+        setIsWaitingForPeer(false); // Reset the flag after making a call
       } else {
+        // If no peer is found, set a timeout to try again after a delay
+        setTimeout(() => {
+          setIsWaitingForPeer(false);
+        }, 5000); // Wait for 5 seconds before trying again
         console.log('Waiting for another peer.');
       }
     } catch (error) {
       console.error('Error fetching peer ID:', error);
-    } finally {
-      setIsWaitingForPeer(false);
+      setIsWaitingForPeer(false); // Ensure flag is reset on error as well
     }
   };
-
   const makeCall = (id) => {
     const call = peer.call(id, myStream);
     call.on('stream', handleStream);
